4dc4c487926f3114f71740cb8d5b72641a7a8038
Roman Romanov <r.romanov.mail@yandex.ru>
2014-10-22 16:29:49 +0400

    Main bundle structure

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Intaro\FileUploaderBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('intaro_file_uploader');
+
+        return $treeBuilder;
+    }
+}
