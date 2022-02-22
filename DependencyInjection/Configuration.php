c3d239d143e52e52ae3e592dc4beac03cf6bd7d7
Alexey <ad.defas@gmail.com>
2022-02-22 16:46:31 +0300

    fix deprecations for symfony >= 4.2
    A tree builder without a root node is deprecated since Symfony 4.2 and will not be supported anymore in 5.0.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,8 +9,14 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('intaro_file_uploader');
+        $treeBuilder = new TreeBuilder('intaro_file_uploader');
+        if (\method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('intaro_file_uploader');
+        }
+        
         $rootNode
             ->children()
                 ->arrayNode('uploaders')
