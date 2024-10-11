c5406108d5e4293044c96f7714824b4985a0ea43
Ilyas Salikhov <salikhoff@gmail.com>
2024-10-11 13:44:03 +0300

    fix Symfony deprecation notice

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -7,7 +7,7 @@ use Symfony\Component\Config\Definition\ConfigurationInterface;
 
 class Configuration implements ConfigurationInterface
 {
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('intaro_file_uploader');
         if (\method_exists($treeBuilder, 'getRootNode')) {
